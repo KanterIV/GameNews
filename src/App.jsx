a925b02e1661b178cbc0e35093278e6f5441e44b
KanterIV <126972573+KanterIV@users.noreply.github.com>
2025-09-02 10:42:48 +0200

    add postmessage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from 'react';
 import { mockNews } from './constans/mockNews'
 import NewsList from './components/NewsList/NewsList'
 
 function App() {
 
+  useEffect(() => {
+    if (window.App?.postMessage) {
+      window.App.postMessage("news");
+    }
+  }, []);
+  
   return (
     <main className='container'>
       <NewsList newsArray={mockNews} />
